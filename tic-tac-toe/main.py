c8c17150642195ab4e27da2cde9b8cb4beef80b5
Diogo Guedes <diogo.guedes111@gmail.com>
2024-10-07 08:35:58 +0100

    tic tac toe Graphical interface

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,3 +1,28 @@
 # Tic tac toe game
 
+from tkinter import *
 
+class TicTacToe:
+
+  def __init__(self):
+    root = Tk()
+    root.title("Tic-Tac-Toe Game")
+    self.create_grid(root)
+    root.mainloop()
+
+  def create_grid(self,root):
+    for row in range(3):
+      for col in range(3):
+        btn = Button(root,text="",font="normal 20 bold",width=5,height=2)
+        btn.grid(row=row,column=col)
+  
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  game = TicTacToe()
