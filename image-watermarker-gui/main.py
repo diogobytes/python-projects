1903c1106c8e6713f165b69d8bd62be6df1a19e7
Diogo Guedes <diogo.guedes111@gmail.com>
2024-10-11 17:50:00 +0100

    chore(image-watermark-gui): watermark text

--- image-watermarker-gui/main.py
+++ image-watermarker-gui/main.py
@@ -1,1 +1,19 @@
 # Desktop  Application with a GUI where you can upload an image and use python to add a watermark logo/text.
+
+# https://pypi.org/project/Pillow/
+#https://docs.python.org/3/library/tkinter.html
+
+from PIL import Image,ImageDraw, ImageFont
+
+
+
+
+if __name__ == '__main__':
+  im = Image.open('./matrix.jpg')
+  watermark_im = im.copy()
+  fnt = ImageFont.truetype("./Arial.ttf", 40)
+
+  draw = ImageDraw.Draw(watermark_im)
+  draw.text((0,0),"Diogo Guedes",fill=(0, 0, 0, 0),font=fnt)
+  watermark_im.show()
+
