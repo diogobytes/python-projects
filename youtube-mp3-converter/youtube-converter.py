0e699ec99b59d0b3118117bcb3d58e77dad0d638
Diogo Guedes <diogo.guedes111@gmail.com>
2024-10-16 12:04:43 +0100

    chore(youtube-mp3-converter): initialize project

---
+++ youtube-mp3-converter/youtube-converter.py
@@ -0,0 +1,25 @@
+import sys
+
+
+class YoutubeConverter:
+  def __init__(self):
+    print("[*] Welcome to Youtube Converter to MP3")
+    print("[*] Usage: python3 youtube-converter.py {--playlist} {--link}")
+    
+
+def convert_link():
+  print
+
+def convert_playlist():
+  print
+
+
+
+def main():
+  print("[*] Welcome to ")
+
+
+
+
+if __name__ == '__main__':
+  YoutubeConverter()
